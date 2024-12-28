18db84983f346b23090667b20e3f9c7d5c6c85f0
gogwangsin <gwangsin1029@naver.com>
2024-12-28 15:24:30 +0900

    임시 파일 재빌드 및 BattleGameMode C++

---
+++ Project_Animagus/Source/Project_Animagus/BattleGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BattleGameMode.h"
+
+void BattleGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    UE_LOG(LogTemp, Display, TEXT("Battle Game Mode Begin"));
+}
+
+
