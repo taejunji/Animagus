882acef8436fe36531d8225dc2c261ced714f269
gogwangsin <gwangsin1029@naver.com>
2025-01-21 22:10:46 +0900

    Blend Space 수정

--- Project_Animagus/Source/Project_Animagus/Character/BaseCharacter.cpp
+++ Project_Animagus/Source/Project_Animagus/Character/BaseCharacter.cpp
@@ -18,7 +18,7 @@ void ABaseCharacter::BeginPlay()
     // 기본 걷기 속도 300으로 설정 - 1초에 3m
     GetCharacterMovement()->JumpZVelocity = 500.0f;
 
-    default_walk_speed = 300.f;
+    default_walk_speed = 250.f;
     default_run_speed = 500.f;
     speed_change_rete = 5.f; // 1초에 5.f정도의 속도 변화를 꿈 꿨는데 뭔가 이상하다 
     current_speed = default_walk_speed;
