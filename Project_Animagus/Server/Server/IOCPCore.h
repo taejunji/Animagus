418ff69533a0aa722a19e12514cf75bc2df5b9d2
chohwanhee(desktop) <great1625@gmail.com>
2025-02-13 21:05:54 +0900

    프로토콜 및 pch 파일 작성

---
+++ Project_Animagus/Server/Server/IOCPCore.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "pch.h"
+
+
+class IOCPCore
+{
+    IOCPCore();
+    ~IOCPCore();
+
+    void Run();
+    void Disconnect();
+
+};
+
---
+++ Project_Animagus/Server/Server/protocol.h
@@ -0,0 +1,49 @@
+#pragma once
+
+enum class PlayerType
+{
+    Tiger,
+    Ram,
+    Monkey,
+    Koala,
+};
+
+enum class SkillID
+{
+    Heal,           // 힐
+    EnergyWave,     // 에너지파
+    BlackHole,      // 블랙홀
+    Silence,        // 침묵
+    GlacialPath,    // 빙판길
+    Fireball,       // 화염구
+    Invisibility,   // 은신
+    Teleport,       // 텔레포트
+    Shield,         // 방어막
+    LifeDrain,      // 흡혈
+    Shockwave,      // 기폭발
+    Thunderstrike,  // 천둥
+};
+
+
+
+
+#pragma pack (push, 1)
+struct CS_MOVE_PKT
+{
+    unsigned char size;
+    unsigned char type;
+    int	p_id;
+    float Max_speed;
+    float x, y, z;
+    float vx, vy, vz;
+    float yaw, pitch;
+};
+struct CS_SELECT_CHARACTER
+{
+    unsigned char size;
+    unsigned char type;
+    int id;
+    float x, y, z;
+    PlayerType p_type;
+};
+#pragma pack(pop)
