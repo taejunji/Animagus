d55ba1b245e005d63aede52cd93f48ef84e34285
gogwangsin <gwangsin1029@naver.com>
2025-05-02 00:12:13 +0900

    AI MoveTo : move to에서 네비 메쉬가 아닌 곳을 찾아가려하거나 길을 찾을 수 없을 때 정체가 생기는 오류를 Service 노드로 위치 변화량을 확인해서 해제 시키게 함,

--- Project_Animagus/Source/Project_Animagus/AI/MyAIController.cpp
+++ Project_Animagus/Source/Project_Animagus/AI/MyAIController.cpp
@@ -68,6 +68,9 @@ void AMyAIController::BeginPlay()
             // AIStateKey.SelectedKeyName = FName(TEXT("AIState"));
             // BlackboardPtr->SetValueAsEnum(AIStateKey.SelectedKeyName, static_cast<uint8>(EAIState::Patrol));
 
+            can_set_target_key.SelectedKeyName = FName(TEXT("CanSetTarget"));
+            BlackboardPtr->SetValueAsBool(can_set_target_key.SelectedKeyName, true);
+
             DefendRadiusKey.SelectedKeyName = FName(TEXT("DefendRadius"));
             BlackboardPtr->SetValueAsFloat(DefendRadiusKey.SelectedKeyName, 600.f); // 350
 
@@ -273,7 +276,9 @@ void AMyAIController::CheckAndDisableTargetIfDead()
 
 void AMyAIController::OnPerceptionUpdated(const TArray<AActor*>& UpdatedActors)
 {
-    if (bCanChangeTarget == false) return; // 타겟 변경이 불가능한 경우 나가기
+    bool bCanSet = GetBlackboardComponent()->GetValueAsBool(can_set_target_key.SelectedKeyName);
+
+    if (bCanSet == false) return; // 타겟 변경이 불가능한 경우 나가기
 
     TSet<AActor*> NewlySensedActors; 
     TSet<AActor*> CandidateTargets; // 후보군 타겟들을 저장할 집합 (중복 방지) 
@@ -424,7 +429,8 @@ float AMyAIController::CalculateTargetPriority(ABaseCharacter* TargetCharacter)
 
 void AMyAIController::ResetTargetChange()
 {
-    bCanChangeTarget = true;
+    GetBlackboardComponent()->SetValueAsBool(can_set_target_key.SelectedKeyName, true);
+    // bCanChangeTarget = true;
 }
 
 void AMyAIController::RememberLostTarget(AActor* Target)
@@ -477,7 +483,9 @@ void AMyAIController::SetAITarget(ABaseCharacter* NewTarget)
         GetBlackboardComponent()->SetValueAsObject(TargetKey.SelectedKeyName, NewTarget);
 
         // 3초 동안 타겟 변경 불가능
-        bCanChangeTarget = false;
+        // bCanChangeTarget = false;
+        GetBlackboardComponent()->SetValueAsBool(can_set_target_key.SelectedKeyName, false);
+
         GetWorld()->GetTimerManager().SetTimer(TargetChangeTimerHandle, this, &AMyAIController::ResetTargetChange, 10.0f, false);
     }
     else
@@ -523,7 +531,7 @@ void AMyAIController::CheckAndRecoverFromNavMesh()
         //bCanChangeTarget = true;
 
         // 네비메시를 찾지 못한 상태를 기록
-        // bFailedToFindNavMesh = true;
+        bFailedToFindNavMesh = true;
     }
     else {
         // 디버그 구 그리기
